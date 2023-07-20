aa1770fdca90ff81ce2c93f341429f3ff59b9625
raulmunozrayo <munozrayo.raul@gmail.com>
2023-07-20 14:11:37 +0200

    inclusion boton like

---
+++ like.js
@@ -0,0 +1,34 @@
+let likesCount = 0;
+let hasLiked = false;
+const likeButton = document.querySelector('.like-button');
+const notificationsList = document.getElementById('notifications-list');
+
+likeButton.addEventListener('click', toggleLike);
+
+function toggleLike() {
+  if (!hasLiked) {
+    hasLiked = true;
+    likeButton.classList.add('liked');
+    likesCount++;
+    showNotification('¡Has valorado positivamente al trabajador!');
+  }
+//   else {
+//     hasLiked = false;
+//     likeButton.classList.remove('liked');
+//     likesCount--;
+//   }
+}
+  
+
+  likesCountElement.textContent = likesCount;
+
+function showNotification(message) {
+  const notification = document.createElement('li');
+  notification.textContent = message;
+  notificationsList.appendChild(notification);
+
+  // Eliminar la notificación después de 3 segundos
+  setTimeout(() => {
+    notification.remove();
+  }, 3000);
+}
