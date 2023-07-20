5de10b9207f290ba44c7c86ea7450c9e7b429286
Neema <106617302+shedevopsbcn@users.noreply.github.com>
2023-07-20 09:55:50 +0200

    favoritos

---
+++ favoritos.js
@@ -0,0 +1,29 @@
+function getMyFavorites() {
+  
+    let favoriteFact = document.getElementById('selected-useless-fact').innerText
+    console.log(favoriteFact);
+    arrayFavFacts.push(favoriteFact);
+    let newFavItem = arrayFavFacts[arrayFavFacts.length-1];
+  
+    printFavorite(newFavItem);
+  
+  }
+  
+  
+    function printFavorite(string) {
+      const favoritesList = document.getElementsByTagName("ul")[0]
+    
+        let listItem = document.createElement('li');
+        listItem.className = 'list-items';
+        listItem.innerText = string;
+            
+            favoritesList.appendChild(listItem);
+        }
+
+        
+module.exports = {
+printFavorite, 
+getUselessFact,
+getMyFavorites   
+};
+  
