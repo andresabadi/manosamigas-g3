04e1dc1fce722c72234303db4578c1d1755c53dc
cesarconte <yugurta@gmail.com>
2023-07-20 23:00:09 +0200

    some small HTML and CSS adjustments

--- script/banner-search.js
+++ script/banner-search.js
@@ -1,27 +1,4 @@
-/*const carousel = document.querySelector('.carousel');
-const images = document.querySelectorAll('.carousel img');
 
-const totalImages = images.length;
-const imageWidth = images[0].clientWidth;
-let currentIndex = 0;
-
-function moveCarousel() {
-  carousel.style.transition = 'transform 0.5s ease-in-out';
-  carousel.style.transform = `translateX(-${imageWidth * currentIndex}px)`;
-}
-
-function showNextImage() {
-  currentIndex = (currentIndex + 1) % totalImages;
-  moveCarousel();
-}
-
-// Mostramos la primera imagen
-moveCarousel();
-
-// Intervalo para cambiar automáticamente las imágenes cada 3 segundos
-setInterval(showNextImage, 3000); */
-
-//BUSCADOR//
 function searchKeyword() {
   const keyword = document.getElementById('keyword').value.toLowerCase();
   const elementsToSearch = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6');
