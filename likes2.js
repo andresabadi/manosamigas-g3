62de8f734b5b81bc84fde66b2711744afed2f9b3
raulmunozrayo <munozrayo.raul@gmail.com>
2023-07-20 18:18:04 +0200

    adaptacionlike

--- likes2.js
+++ likes2.js
@@ -1,37 +1,41 @@
-let likesCount = 0;
-let hasLiked = false;
-const likeButton = document.querySelector('.like-button');
-const likesCountElement = document.querySelector('.likes-count');
-const notificationsList = document.getElementById('notifications-list');
-const notificationMessage = document.querySelector('.notification-message');
+const cards = document.querySelectorAll('.card_services_likes_container');
 
-likeButton.addEventListener('click', toggleLike);
+cards.forEach(card => {
+  let likesCount = 0;
+  let hasLiked = false;
+  const likeButton = card.querySelector('.like-button');
+  const likesCountElement = card.querySelector('.likes-count');
+  const notificationsList = card.querySelector('.notifications-list');
+  const notificationMessage = card.querySelector('.notification-message');
 
-function toggleLike() {
-  if (!hasLiked) {
-    hasLiked = true;
-    likeButton.classList.add('liked');
-    likesCount++;
-    updateLikes();
-    showNotification('¡Has valorado positivamente al trabajador!');
-  } else {
-    hasLiked = false;
-    likeButton.classList.remove('liked');
-    likesCount--;
-    updateLikes();
-    hideNotification();
+  likeButton.addEventListener('click', toggleLike);
+
+  function toggleLike() {
+    if (!hasLiked) {
+      hasLiked = true;
+      likeButton.classList.add('liked');
+      likesCount++;
+      updateLikes();
+      showNotification('¡Has valorado positivamente al trabajador!');
+    } else {
+      hasLiked = false;
+      likeButton.classList.remove('liked');
+      likesCount--;
+      updateLikes();
+      hideNotification();
+    }
   }
-}
 
-function updateLikes() {
-  likesCountElement.textContent = likesCount;
-}
+  function updateLikes() {
+    likesCountElement.textContent = likesCount;
+  }
 
-function showNotification(message) {
-  notificationMessage.style.display = 'block';
-  notificationMessage.textContent = message;
-}
+  function showNotification(message) {
+    notificationMessage.style.display = 'block';
+    notificationMessage.textContent = message;
+  }
 
-function hideNotification() {
-  notificationMessage.style.display = 'none';
-}
+  function hideNotification() {
+    notificationMessage.style.display = 'none';
+  }
+});
